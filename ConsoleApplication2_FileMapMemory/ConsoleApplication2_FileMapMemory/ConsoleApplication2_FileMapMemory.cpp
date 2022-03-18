a6bb017e8b30c7ba0bfdd95ce55fa3bd979e0373
he55 <40764272+he55@users.noreply.github.com>
2022-03-18 16:22:17 +0800

    add

---
+++ ConsoleApplication2_FileMapMemory/ConsoleApplication2_FileMapMemory/ConsoleApplication2_FileMapMemory.cpp
@@ -0,0 +1,47 @@
+// ConsoleApplication2_FileMapMemory.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <Windows.h>
+
+void FileReverse(PCTSTR pszPathName);
+void todo(PVOID pv) {
+	char* str = (char*)pv;
+	return;
+}
+
+
+int main()
+{
+	FileReverse(L"C:\\Users\\qwe\\Downloads\\123.txt");
+}
+
+void FileReverse(PCTSTR pszPathName)
+{
+	HANDLE hFile = CreateFile(pszPathName, GENERIC_WRITE | GENERIC_READ, 0, NULL
+		, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (hFile == INVALID_HANDLE_VALUE)
+	{
+		printf("File could not be opened.");
+		return;
+	}
+
+	DWORD dwFileSize = GetFileSize(hFile, NULL);
+
+	HANDLE hFileMap = CreateFileMapping(hFile, NULL, PAGE_READWRITE, 0,
+		dwFileSize + sizeof(char), NULL);
+
+	if (hFileMap == NULL) {
+		CloseHandle(hFile);
+		return;
+	}
+
+	PVOID pvFile = MapViewOfFile(hFileMap, FILE_MAP_READ, 0, 0, 0);
+	if (pvFile) {
+		todo(pvFile);
+		UnmapViewOfFile(pvFile);
+	}
+
+	CloseHandle(hFileMap);
+	CloseHandle(hFile);
+}
