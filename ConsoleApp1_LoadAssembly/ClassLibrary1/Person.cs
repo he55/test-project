156d24237df57c5186d6449c4a505391f72c2ac3
he55 <40764272+he55@users.noreply.github.com>
2022-07-26 21:44:32 +0800

    add load assembly

---
+++ ConsoleApp1_LoadAssembly/ClassLibrary1/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Person
+    {
+        public int Id { get; set; } = 12;
+        public string Name { get; set; } = "lili";
+
+        public string GetName()
+        {
+            return Name;
+        }
+    }
+}
---
+++ ConsoleApp1_LoadAssembly/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Type type1=null;
+            Assembly assembly= Assembly.UnsafeLoadFrom(@"C:\Users\luckh\source\repos\ConsoleApp1\ClassLibrary1\bin\Debug\net472\ClassLibrary1.dll");
+            foreach (Type type in assembly.GetExportedTypes())
+            {
+                if (!type.IsAbstract && type.IsPublic && type.IsClass && type.Name== "Person")
+                {
+                    type1 = type;
+                    break;
+                }
+            }
+
+            object instance = (object)Activator.CreateInstance(type1);
+            MethodInfo methodInfo = type1.GetMethod("GetName");
+            object v = methodInfo.Invoke(instance, null);
+        }
+    }
+}
