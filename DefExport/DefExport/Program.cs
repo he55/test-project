22d000ab0f71a2000347489373a1e6a7d6485c1f
he55 <40764272+he55@users.noreply.github.com>
2022-08-06 23:47:44 +0800

    Update Program.cs

--- DefExport/DefExport/Program.cs
+++ DefExport/DefExport/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace DefExport
 {
@@ -6,6 +8,61 @@ namespace DefExport
     {
         static void Main(string[] args)
         {
+            string p = @"C:\Users\luckh\Desktop\exports.txt";
+            StreamReader streamReader = File.OpenText(p);
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 9)
+                    return;
+
+                string v = streamReader.ReadLine();
+                int v1 = v.IndexOf("File Type: DLL");
+                if (v1 > 0)
+                    break;
+            }
+
+            int num = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 9)
+                    return;
+
+                string v = streamReader.ReadLine();
+                int v1 = v.IndexOf("number of functions");
+                if (v1 > 0)
+                {
+                    string v2 = v.Substring(0, v1);
+                    num= int.Parse(v2);
+                    break;
+                }
+            }
+
+            int idx = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 9)
+                    return;
+
+                string v = streamReader.ReadLine();
+                int v1 = v.IndexOf("ordinal");
+                if (v1 > 0)
+                {
+                    idx= v.IndexOf("name");
+                    streamReader.ReadLine();
+                    break;
+                }
+            }
+
+            List<string> syms = new List<string>();
+            for (int i = 0; i < num; i++)
+            {
+                string v = streamReader.ReadLine();
+                string v1 = v.Substring(idx);
+                syms.Add(v1);
+            }
+
+
+            streamReader.Close();
         }
     }
 }
