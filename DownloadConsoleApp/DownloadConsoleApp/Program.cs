ee5ac3872011a43fee0b7f6dee6a2cc599881809
he55 <40764272+he55@users.noreply.github.com>
2022-10-03 23:09:30 +0800

    add download

---
+++ DownloadConsoleApp/DownloadConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace DownloadConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WebClient webClient = new WebClient();
+            string[] urls = File.ReadAllLines("dl.txt");
+            foreach (var url in urls)
+            {
+                Uri uri=new Uri(url);
+                string host = uri.Host;
+                string apath=uri.AbsolutePath.Substring(1);
+                string fpath=apath.Substring(0,apath.LastIndexOf('/'));
+                if(!Directory.Exists(fpath))
+                    Directory.CreateDirectory(fpath);
+
+                webClient.DownloadFile(url, apath);
+            }
+        }
+    }
+}
