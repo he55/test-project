10b3ffe3d5f469ed35a0b746174e734649685cf6
he55 <40764272+he55@users.noreply.github.com>
2022-10-03 13:57:08 +0800

    add hook 2

---
+++ WindowsFormsApp1_Hook2/WindowsFormsApp1/Program.cs
@@ -0,0 +1,72 @@
+﻿using PlayHooky;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    static class Program
+    {
+        //We take the TagetClass "this" as our first argument. Don't forget this, it's important!
+        //Note that this method is static. This is important.
+        public static string addhook(object t, string str)
+        {
+            //manager2.Unhook(methodInfo2);
+            //object v = methodInfo2.Invoke(t, new object[] { str });
+            //manager2.Hook(methodInfo2, typeof(Program).GetMethod("addhook"));
+
+            string vv = oriaddhook(t, str);
+
+
+            string cn = "Hook";
+            if (str.Contains("Window"))
+                return cn;
+            else
+                return cn +"."+ str;
+        }
+
+        public static string oriaddhook(object t, string str)
+        {
+            return null;
+            //string cn = "Hook";
+            //if (str.Contains("Window"))
+            //    return cn;
+            //else
+            //    return cn + "." + str;
+        }
+
+        static  System.Reflection.MethodInfo methodInfo2;
+       static HookManager manager2;
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //Create the HookManager -- make sure this is done thread safe!
+            HookManager manager = new HookManager();
+            manager2 = manager;
+
+            Type type = typeof(System.Windows.Forms.NativeWindow);
+            string assemblyQualifiedName = type.AssemblyQualifiedName;
+
+            Type type1 = Type.GetType("System.Windows.Forms.NativeWindow+WindowClass, System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089");
+            System.Reflection.MethodInfo methodInfo = type1.GetMethod("GetFullClassName", System.Reflection.BindingFlags.NonPublic| System.Reflection.BindingFlags.Instance);
+            methodInfo2 = methodInfo;
+
+            //Hook our target method
+            manager.Hook(methodInfo, typeof(Program).GetMethod("addhook"), typeof(Program).GetMethod("oriaddhook"));
+
+
+            //Unhook the target method
+            //manager.Unhook(methodInfo);
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
